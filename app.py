27bd775240c7e6916a7b31b58d9fc03dea9edc85
snaplucas <lucas.chapola@gmail.com>
2018-02-23 20:20:29 -0300

    update

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask('python_api')
 
 @app.route("/")
 def hello_world():
     return "Hello World! <strong>I am learning Flask</strong>", 200
+
+@app.route("/<name>")
+def index(name):
+    if name.lower() == "bruno":
+        return "Olá {}".format(name), 200
+    else:
+        return "Not Found", 404
 
 app.run()
